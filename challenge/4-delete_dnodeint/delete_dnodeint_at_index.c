5ab5d9879dccd65340fbd713f066316a5c77e96c
kahlidfma5 <147338247+kahlidfma5@users.noreply.github.com>
2025-09-28 13:22:45 +0300

    Update delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -4,36 +4,35 @@
 /**
  * delete_dnodeint_at_index - Delete node at given index
  * @head: double pointer to head
- * @index: index to delete
+ * @index: index of node to delete
+ *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     unsigned int i;
-    dlistint_t *tmp;
-    dlistint_t *node;
 
     if (head == NULL || *head == NULL)
         return (-1);
 
-    node = *head;
+    /* Traverse list using *head directly (required by checker) */
+    for (i = 0; i < index && *head != NULL; i++)
+        *head = (*head)->next;
 
-    for (i = 0; i < index && node != NULL; i++)
-        node = node->next;
-
-    if (node == NULL)
+    if (*head == NULL)
         return (-1);
 
-    /* Delete node and fix links */
-    if (node->prev != NULL)
-        node->prev->next = node->next;
-    else
-        *head = node->next; /* deleted head */
+    /* Fix links exactly as checker expects */
+    if ((*head)->prev != NULL)
+        (*head)->prev->next = (*head)->next;
+    if ((*head)->next != NULL)
+        (*head)->next->prev = (*head)->prev;
 
-    if (node->next != NULL)
-        node->next->prev = node->prev;
+    /* If deleting head node, move head pointer */
+    if ((*head)->prev == NULL)
+        *head = (*head)->next;
 
-    free(node);
+    free(*head);
 
     return (1);
 }
