6126cfb3a5f72a1673fdc5a3a8648495daa1b9cd
kahlidfma5 <147338247+kahlidfma5@users.noreply.github.com>
2025-09-28 13:13:10 +0300

    Update delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -4,51 +4,48 @@
 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
- *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
+ * @head: Double pointer to the head of the list
+ * @index: The index of the node to delete (0-based)
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+    dlistint_t *tmp, *to_delete;
+    unsigned int p = 0;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-	saved_head = *head;
-	p = 0;
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
-		*head = saved_head;
-	}
-	return (1);
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    tmp = *head;
+
+    /* Traverse to the node at the given index */
+    while (p < index && tmp != NULL)
+    {
+        tmp = tmp->next;
+        p++;
+    }
+
+    if (tmp == NULL) /* index out of range */
+        return (-1);
+
+    to_delete = tmp;
+
+    /* If we're deleting the head node */
+    if (index == 0)
+    {
+        *head = to_delete->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+    }
+    else
+    {
+        if (to_delete->prev != NULL)
+            to_delete->prev->next = to_delete->next;
+        if (to_delete->next != NULL)
+            to_delete->next->prev = to_delete->prev;
+    }
+
+    free(to_delete);
+    return (1);
 }
